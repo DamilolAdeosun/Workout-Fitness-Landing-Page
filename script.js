d406983d20f8d6a61344d77616c92ec4f2adaa99
DamilolAdeosun <137585624+DamilolAdeosun@users.noreply.github.com>
2025-06-28 14:13:11 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Toggle mobile menu
+const navToggle = document.getElementById('navToggle');
+const navMenu = document.getElementById('navMenu');
+
+navToggle.addE// Toggle mobile menu
+const navToggle = document.getElementById('navToggle');
+const navMenu = document.getElementById('navMenu');
+
+navToggle.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
+sList.toggle('active');
+});
